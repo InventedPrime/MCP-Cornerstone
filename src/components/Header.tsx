9016ffa73fcb0bb77a3f55ceb66a6032715df16d
Invented Prime <dlcgmike@gmail.com>
2026-06-27 15:41:16 -0400

    test: deploy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,48 @@
 // import { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { LogInButton, SignUpButton, LogOutButton } from './SignInOut';
-import { useAuth } from '../context/AuthContext';
-import { Loader } from './Loader';
+import { Link, useLocation } from "react-router-dom";
+import { LogInButton, SignUpButton, LogOutButton } from "./SignInOut";
+import { useAuth } from "../context/AuthContext";
+import { Loader } from "./Loader";
 
 export const Header = () => {
-    const { user, loading } = useAuth();
-    const location = useLocation();
-    return (
-        <>
-        <Loader isLoading={loading}/>
-        <header>
-            <div className='header-container-logo'>
-                <img src="/MCPLogo.png" alt="MCP Logo" />
-                <h1>MCP Cornerstone</h1>
-            </div>
-            <div className='header-container-nav'>
-                    <Link 
-                        style={{
-                            color: location.pathname =='/Home' ? 'var(--light-blue)' : 'white',
-                        }} 
-                        to={'/Home'}
-                    >
-                        Home
-                    </Link>
-                    <Link 
-                        style={{
-                            color: location.pathname == '/Dashboard' ? 'var(--light-blue)' : 'white',
-                        }} 
-                        to={'/Dashboard'}
-                    >
-                        Dashboard
-                    </Link>
-            </div>
-            <div className='header-container-sign'>
-            <p>
-              { user ? user.displayName : "Guest"}</p>
-              { user ? <LogOutButton  /> : <SignUpButton  />}
-              { user ? null : <LogInButton />}
-            </div>
-        </header>
-        </>
-    )
-}
+  const { user, loading } = useAuth();
+  const location = useLocation();
+  return (
+    <>
+      <Loader isLoading={loading} />
+      <header>
+        <div className="header-container-logo">
+          <img src="/MCPLogo.png" alt="MCP Logo" />
+          <h1>MCP Cornerstones</h1>
+        </div>
+        <div className="header-container-nav">
+          <Link
+            style={{
+              color:
+                location.pathname == "/Home" ? "var(--light-blue)" : "white",
+            }}
+            to={"/Home"}
+          >
+            Home
+          </Link>
+          <Link
+            style={{
+              color:
+                location.pathname == "/Dashboard"
+                  ? "var(--light-blue)"
+                  : "white",
+            }}
+            to={"/Dashboard"}
+          >
+            Dashboard
+          </Link>
+        </div>
+        <div className="header-container-sign">
+          <p>{user ? user.displayName : "Guest"}</p>
+          {user ? <LogOutButton /> : <SignUpButton />}
+          {user ? null : <LogInButton />}
+        </div>
+      </header>
+    </>
+  );
+};
